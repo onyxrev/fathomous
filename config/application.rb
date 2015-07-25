f8cce8c913272abc00a0a152634c16b61a7a1efd
Dan Connor <danconn@danconnor.com>
2015-07-25 03:06:07 -0400

    initial trailblazer and some gems

--- config/application.rb
+++ config/application.rb
@@ -24,3 +24,5 @@ module Fathomous
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+require 'trailblazer/rails/railtie'
---
+++ config/initializers/trailblazer.rb
@@ -0,0 +1,11 @@
+# I extend the CRUD module here to make it also include CRUD::ActiveModel globally. This is my choice as the
+# application architect. Don't do it if you don't use ActiveModel form builders/models.
+Trailblazer::Operation::CRUD.module_eval do
+  module Included
+    def included(base)
+      super # the original CRUD::included method.
+      base.send :include, Trailblazer::Operation::CRUD::ActiveModel
+    end
+  end
+  extend Included # override CRUD::included.
+end
